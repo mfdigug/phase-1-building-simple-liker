ee0e8830702088b3e7f587cfd9a3b819fe869fb0
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-05-24 13:52:53 +1000

    done with simple liker app

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//function likerFunction(){
+
+
+
+function addEventListenerToHeartButton(){
+  //get element
+const buttons = document.getElementsByClassName('like-glyph')
+  for(let btn of buttons){
+    btn.addEventListener('click', (event) => {
+      mimicServerCall()
+       // handle successful response by altering the heart graphic
+      .then((response) => {
+        if (btn.innerText === "&#x2661;") {
+          btn.innerText = "♥";
+          btn.classList.add('activated-heart');
+        } else if (btn.innerText === "♡"){
+          btn.innerText = "♥";
+          btn.classList.add('activated-heart');
+        } else {
+          btn.innerText = "♡"
+          btn.classList.remove('activated-heart');
+        }
+     })
+      // catch the server error
+      .catch(function (error) {
+        //remove hidden class on the error message modal
+        const errorMessage = document.getElementById('modal');
+        errorMessage.classList.remove("hidden");
+        //use setTimeout to hide the modal after 3 seconds (readd the .hidden class)
+        setTimeout(()=>{
+          errorMessage.classList.add("hidden");
+        } , 3000) 
+      })
+    })
+    }
+  }
+
+addEventListenerToHeartButton();
 
 
 
@@ -23,3 +64,6 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
+})
